0597997dbab6efd4dd9f1da33ad535f95de933e4
NicoleyuC <43199808+NicoleyuC@users.noreply.github.com>
2018-09-20 00:22:30 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Main extends Applet {
+    public void paint(Graphics g){
+        g.drawString("i want to sleep!damn!",50,50);
+    }
+}
